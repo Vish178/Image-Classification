90d84aa52bae847f842e329ede605f692b29c97a
Vishal Kaushal <kaushalvishal83@gmail.com>
2025-11-03 12:25:04 +0530

    shifted to facenet from haar transformation

--- server/server.py
+++ server/server.py
@@ -2,8 +2,8 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/main")
-def main():
+@app.route("/clf_img", methods=["GET", "POST"])
+def clf_img():
     return "Hello, this is the Image Classification Server!"
 
 if __name__ == '__main__':
